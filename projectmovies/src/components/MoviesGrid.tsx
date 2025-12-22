5acc2fee83b103af712a3dc89bdb277a432b894d
mosabty <s12428761@stu.najah.edu>
2025-12-22 23:22:31 +0200

    finish update

--- projectmovies/src/components/MoviesGrid.tsx
+++ projectmovies/src/components/MoviesGrid.tsx
@@ -1,25 +1,35 @@
 import type {Movie}  from "../containers/WatchPage";
 
 type Props = {
-  movies: Movie[];
-  onWatch: (movie: Movie) => void;
+    movies: Movie[];
+    onWatch: (movie: Movie) => void;
 };
 
 function MoviesGrid({ movies, onWatch }: Props) {
-  return (
-      <main className="movies-grid" id="moviesGrid">
-        {movies.length === 0 && <div>No movies found.</div>}
-        {movies.map((movie) => (
-            <div className="movie-card" key={movie.id}>
-              <div className="poster">
-                <img src={movie.poster} alt={movie.title} loading="lazy"/>
+    return (
+        <main className="movies-grid" id="moviesGrid">
+            {movies.length === 0 && <div>No movies found.</div>}
+            {movies.map((movie) => (
+                <div className="movie-card" key={movie.id}>
+                    <div className="poster">
+                        <img src={movie.poster} alt={movie.title} loading="lazy"/>
 
-              </div>
+                    </div>
+                    <div className="movie-title">{movie.title}</div>
+                    <div style={{color: "#97a6bf", fontSize: 12}}>
+                        {movie.genres && movie.genres.length > 0
+                            ? movie.genres.join(" • ")
+                            : "No genres"}
+                    </div>
+                    <div className="movie-actions">
+                        <button className="watch" onClick={() => onWatch(movie)}>
+                            Watch
+                        </button>
+                    </div>
+                </div>
+            ))}
+        </main>
 
-            </div>
-        ))}
-      </main>
-
-  );
+    );
 }
 export default MoviesGrid;
--- projectmovies/src/containers/WatchPage.tsx
+++ projectmovies/src/containers/WatchPage.tsx
@@ -1,89 +1,152 @@
+import { useState, useEffect } from "react";
 
-  const [activeGenre, setActiveGenre] = useState("");
-  function handleGenreChange(g: string) {
-    setActiveGenre(g);
-  }
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import Categories from "../components/Categories";
+import MoviesGrid from "../components/MoviesGrid";
+import Footer from "../components/Footer";
+import Pagination from "../components/Pagination";
+import CardWatch from "../components/CardWatch";
 
-  const API_URL ="https://mocki.io/v1/a74b6ea8-4d31-4e4e-a046-2f38ab864f7b";
+export interface Movie {
+    id: number;
+    title: string;
+    genres: string[];
+    length: number;
+    rating: string;
+    lang: string;
+    poster: string;
+    trailer: string;
+    desc: string;
+}
 
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [filteredMovies, setFilteredMovies] = useState<Movie[]>([]);
+
+const WatchPage = () => {
+    const [search, setSearch] = useState("");
+    function handleSearchChange(value: string) {
+        setSearch(value);
+    }
+
+    const [activeGenre, setActiveGenre] = useState("");
+    function handleGenreChange(g: string) {
+        setActiveGenre(g);
+    }
+
+    const API_URL ="https://mocki.io/v1/a74b6ea8-4d31-4e4e-a046-2f38ab864f7b";
+
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [filteredMovies, setFilteredMovies] = useState<Movie[]>([]);
 
 
-  // ============= PAGINATION =========
-  const ITEMS_PER_PAGE = 12;
-  const [currentPage, setCurrentPage] = useState(1);
+    // ============= PAGINATION =========
+    const ITEMS_PER_PAGE = 12;
+    const [currentPage, setCurrentPage] = useState(1);
 
 
-  // ===== Watch Modal ====
-  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+    // ===== Watch Modal ====
+    const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-  // ========== FETCH ==========
-  useEffect(() => {
-    const fetchMovies = async () => {
-      const data = await fetch(API_URL)
-          .then((res) => res.json());
-      setMovies(data);
-      setFilteredMovies(data);
+    // ========== FETCH ==========
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const data = await fetch(API_URL)
+                .then((res) => res.json());
+            setMovies(data);
+            setFilteredMovies(data);
+        };
+
+        fetchMovies();
+    }, []);
+
+    // ========== FILTER ==========
+    useEffect(() => {
+        let list = [...movies];
+
+
+        if (activeGenre) {
+            list = list.filter((m) => m.genres.includes(activeGenre));
+        }
+
+        if (search.trim()) {
+            const q = search.toLowerCase();
+
+            list = list.filter(
+                (m) =>
+                    m.title.toLowerCase().includes(q) ||
+                    m.genres.join(" ").toLowerCase().includes(q)
+            );
+        }
+
+        // تحديث النتائج
+        setFilteredMovies(list);
+
+        // رجّع الصفحة لأول صفحة عند أي تغيير
+        setCurrentPage(1);
+
+    }, [movies, search, activeGenre]);
+
+
+    // ================== PAGINATION LOGIC ==================
+    const totalPages = Math.ceil(filteredMovies.length / ITEMS_PER_PAGE);
+
+    const start = (currentPage - 1) * ITEMS_PER_PAGE;
+    const end = start + ITEMS_PER_PAGE;
+
+    const paginatedMovies = filteredMovies.slice(start, end);
+
+    const handlePrev = () => {
+        setCurrentPage((p) => Math.max(1, p - 1));
     };
 
-    fetchMovies();
-  }, []);
+    const handleNext = () => {
+        setCurrentPage((p) => Math.min(totalPages, p + 1));
+    };
 
-  // ========== FILTER ==========
-  useEffect(() => {
-    let list = [...movies];
+    // ================== WATCH ==================
+    const handleWatch = (movie: Movie) => {
+        console.log("WATCH MOVIE:", movie);
+        setSelectedMovie(movie);
+    };
 
+    return (
 
-    if (activeGenre) {
-      list = list.filter((m) => m.genres.includes(activeGenre));
-    }
-
-    if (search.trim()) {
-      const q = search.toLowerCase();
-
-      list = list.filter(
-          (m) =>
-              m.title.toLowerCase().includes(q) ||
-              m.genres.join(" ").toLowerCase().includes(q)
-      );
-    }
-
-    // تحديث النتائج
-    setFilteredMovies(list);
-
-    // رجّع الصفحة لأول صفحة عند أي تغيير
-    setCurrentPage(1);
-
-  }, [movies, search, activeGenre]);
+        <div className="app">
 
 
+            <Nav
+                search={search}
+                onSearchChange={handleSearchChange}
 
+            />
 
-  return (
+            <Header />
 
-      <div className="app">
+            <Categories
+                activeGenres={activeGenre}
+                onChange={handleGenreChange}
+            />
 
+            {/* الأفلام */}
+            <MoviesGrid movies={paginatedMovies} onWatch={handleWatch} />
 
-        <Nav
-            search={search}
-            onSearchChange={handleSearchChange}
+            {/* الباجينيشن */}
+            {totalPages > 1 && (
+                <Pagination
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    onPrev={handlePrev}
+                    onNext={handleNext}
+                />
+            )}
+            <Footer />
+            {selectedMovie && (
+                <CardWatch
+                    movie={selectedMovie}
+                    onClose={() => setSelectedMovie(null)}
+                />
+            )}
 
-        />
-
-        <Header />
-
-        <Categories
-            activeGenres={activeGenre}
-            onChange={handleGenreChange}
-        />
-
-        {/* الأفلام */}
-        <MoviesGrid movies={paginatedMovies} onWatch={handleWatch} />
-
-        
-
-      </div>
-  );
+        </div>
+    );
 };
 export default WatchPage;
